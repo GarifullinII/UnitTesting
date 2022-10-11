9100e7762b5d2c231a2f690e3a10bbac8032674c
GArifullinII <ildaridrisovich@gmail.com>
2022-10-11 20:58:05 +0400

    feat: add unit tests

--- UnitTestingTests/DataModelTests.swift
+++ UnitTestingTests/DataModelTests.swift
@@ -22,14 +22,32 @@ class DataModelTests: XCTestCase {
         super.tearDown()
     }
 
-    func testExample() {
-       
+    func testLowestVolumeShouldBeZero() {
+        // given (исходные данные, условия для тестирования)
+        sut.setVolume(to: -1)
+        
+        // when (что произойдет, после вызова метода, какое-то изменение)
+        let volume = sut.volume
+        
+        // then (проверка результат)
+        XCTAssert(volume == 0, "Lowest value should be equel zero")
+    }
+    
+    func testHighestVolumeShouldBe100() {
+        // given (исходные данные, условия для тестирования)
+        sut.setVolume(to: 101)
+        
+        // when (что произойдет, после вызова метода, какое-то изменение)
+        let volume = sut.volume
+        
+        // then (проверка результат)
+        XCTAssert(volume == 100, "Highest value should be equel 100")
     }
 
-    func testPerformanceExample() {
-        measure {
-            sut.setVolume(to: 100)
-        }
-    }
+//    func testPerformanceExample() {
+//        measure {
+//            sut.setVolume(to: 100)
+//        }
+//    }
 
 }
